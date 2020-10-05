fe8a63f6873aa73d825781f347cb4e7df1bc3907
gregirie <gregory.futhazar@gmail.com>
2020-10-05 14:26:42 +0200

    - suppress receiver and sender classes due to the introduction of agent class

--- signalling.py
+++ signalling.py
@@ -38,42 +38,6 @@ class Agent:
         self.actions_values[self.last_situation] = optimizer.apply(self.actions_values[self.last_situation], reward)
 
 
-class Sender:
-        
-    def __init__(self, n_inputs: int, n_messages: int, eps: float = 1e-6):
-        self.n_messages = n_messages
-        self.message_weights = np.zeros((n_inputs, n_messages))
-        self.message_weights.fill(eps)
-        self.last_situation = (0, 0)
-        
-    def send_message(self, input: int) -> int:
-        probs = self.message_weights[input, :]/np.sum(self.message_weights[input, :])
-        message = np.random.choice(self.n_messages, p=probs)
-        self.last_situation = (input, message)
-        return message
-
-    def learn_from_feedback(self, reward: int) -> None:
-        self.message_weights[self.last_situation] += reward
-
-
-class Receiver:
-        
-    def __init__(self, n_messages: int, n_actions: int, eps: float = 1e-6):
-        self.n_actions = n_actions
-        self.action_weights = np.ndarray((n_messages, n_actions))
-        self.action_weights.fill(eps)
-        self.last_situation = (0, 0)
-        
-    def act(self, message: int) -> int:
-        probs = self.action_weights[message, :]/np.sum(self.action_weights[message, :])
-        action = np.random.choice(self.n_actions, p=probs)
-        self.last_situation = (message, action)
-        return action
-
-    def learn_from_feedback(self, reward: int) -> None:
-        self.action_weights[self.last_situation] += reward
-
-
 class World:
     def __init__(self, n_states: int, seed: int = 42):
         self.n_states = n_states
